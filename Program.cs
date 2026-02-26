3d93c0152467dea2ee4e395485237b4b0a83921b
ulisses guimaraes <ulisses.guimaraes@rede.ulbra.br>
2026-02-25 21:35:49 -0300

    Add initial project files and assembly information for tde02
    - Created tde02.dll and associated assembly info files.
    - Added global usings and editor config for project settings.
    - Included runtime configuration and assets cache.
    - Generated source link for debugging and tracing.
    - Established project structure with necessary dependencies.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿
+Console.Write("Digite o primeiro número: ");
+double numero_1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+double numero_2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"A soma dos numeros {numero_1} e {numero_2} é igual a {numero_1 + numero_2}");
+
+double sub_nums = numero_1 - numero_2;
+
+Console.WriteLine("A subtração dos números " + numero_1 + " e " + numero_2 + " é igual a " + sub_nums);
+
+double multiplicacao = numero_1 * numero_2;
+
+string mensagem = string.Format("A multiplicação dos números {0} e {1} é igual a {2}", numero_1, numero_2, multiplicacao);
+
+Console.WriteLine(mensagem);
+
+double divisao = numero_1 / numero_2;
+
+Console.WriteLine($"A divisão do numero {numero_1} e {numero_2} é igual a {divisao}");
